6074447c54697ffa154cbc6a431ef320d2a00502
Tony Kay <tony.kay@gmail.com>
2017-10-03 13:20:29 -0700

    updated readme and css ref

--- src/cards/fulcrologic/cards.cljs
+++ src/cards/fulcrologic/cards.cljs
@@ -1,12 +1,16 @@
 (ns fulcrologic.cards
   (:require [devcards.core :refer-macros [defcard-doc defcard]]
             [sablono.core :as html :refer-macros [html]]
+            [fulcrologic.semantic-ui.icons :as i]
             [fulcrologic.semantic-ui.factories :as f]))
 
 (defcard buttons
   (html
     [:div
      (f/ui-button #js {} "A")
-     (f/ui-button #js {} "B")
+     (f/ui-button #js {:content       "Like"
+                       :icon          i/heart-icon
+                       :label         (f/ui-label #js {:as "a" :basic true :pointing "right" :content "2,000"})
+                       :labelPosition "left"})
      (f/ui-button #js {} "C")
      (f/ui-button #js {} "D")]))
